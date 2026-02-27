ef5842340b59fef3e2037d0df8ee6ded3a0585b2
rafa91pinati <rafa91pinati@gmail.com>
2026-02-27 14:47:42 -0300

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyDokOh2iwSG1L6NKna3DuM2jS6YiaphKKM",
-    authDomain: "agenda-4efa7.firebaseapp.com",
-    projectId: "agenda-4efa7",
-    storageBucket: "agenda-4efa7.appspot.com",
-    messagingSenderId: "299659202964",
-    appId: "1:299659202964:web:e358210feb4d7784f63f81"
+    apiKey: "AIzaSyAEQeIKc1MCrV8BJr0CH_mfjwCp1YiRC8s",
+  authDomain: "agenda-4efa7.firebaseapp.com",
+  projectId: "agenda-4efa7",
+  storageBucket: "agenda-4efa7.appspot.com",
+  messagingSenderId: "299659202964",
+  appId: "1:299659202964:web:e358210feb4d7784f63f81"
 });
 
 const messaging = firebase.messaging();
