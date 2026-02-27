47f44b8d96d073356bff2338c68ae29c79e3e26b
rafa91pinati <rafa91pinati@gmail.com>
2026-02-27 13:45:20 -0300

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,25 +3,22 @@ importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-comp
 
 firebase.initializeApp({
     apiKey: "AIzaSyDokOh2iwSG1L6NKna3DuM2jS6YiaphKKM",
-    authDomain: "agenda-4efa7.firebaseapp.com", // 🔥 ADICIONE ISSO
+    authDomain: "agenda-4efa7.firebaseapp.com",
     projectId: "agenda-4efa7",
-    storageBucket: "agenda-4efa7.appspot.com", // 🔥 ADICIONE ISSO
+    storageBucket: "agenda-4efa7.appspot.com",
     messagingSenderId: "299659202964",
     appId: "1:299659202964:web:e358210feb4d7784f63f81"
 });
 
 const messaging = firebase.messaging();
 
-// Ativa imediatamente
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', () => self.clients.claim());
 
-// Mensagem em background
 messaging.onBackgroundMessage((payload) => {
     console.log('Mensagem em segundo plano:', payload);
-
     self.registration.showNotification(payload.notification.title, {
         body: payload.notification.body,
-        icon: '/icon-192.png' // opcional
+        icon: 'https://cdn-icons-png.flaticon.com/512/311/311022.png'
     });
 });
